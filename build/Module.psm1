ba6895f33de38110d93ac6cf69064ae56e26f498
Oleg Sych <oleg_sych@hotmail.com>
2015-05-16 13:41:26 -0700

    - Use Path.Combine in build scripts.

--- build/Module.psm1
+++ build/Module.psm1
@@ -1,14 +1,13 @@
 function CreateAssemblyKeyFile([string] $environmentVariableName, [string] $fileName) {
 	$base64string = [Environment]::GetEnvironmentVariable($environmentVariableName)
 	$bytes = [Convert]::FromBase64String($base64string)
-	$filePath = (Get-Location).Path + $fileName
-	Write-Host "Writing $environmentVariableName to $filePath"
+	$filePath = [IO.Path]::Combine((Get-Location).Path, $fileName)
+	Write-Host "Writing from $environmentVariableName environment variable to $filePath"
 	Set-Content -Path $filePath -Value $bytes -Encoding Byte
 }
 
 function Get-NugetExe() {
-    $currentDirectory = (Get-Location).Path
-    $nugetExe = "$currentDirectory\nuget.exe"
+    $nugetExe = [IO.Path]::Combine((Get-Location).Path, "nuget.exe")
 	Write-Host "Downloading from nuget.org to $nugetExe"
     (new-object Net.WebClient).DownloadFile("http://www.nuget.org/nuget.exe", $nugetExe)
 	return $nugetExe
